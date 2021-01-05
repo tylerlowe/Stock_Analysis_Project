761721935909dc5c996b3e81140210c0ade0da3c
tylerlowe <tylerlowe1998@gmail.com>
2021-01-04 20:30:40 -0500

    Update Downloads_and_Computations.R
    Add Stop Loss

--- Downloads_and_Computations.R
+++ Downloads_and_Computations.R
@@ -7,19 +7,31 @@ library(purrr)
 library(BatchGetSymbols)
 library(RobinHood)
 library(RPostgres)
+library(tibble)
+library(data.table)
+
+rh_user <- Sys.getenv("rh_user")
+rh_pw <- Sys.getenv("rh_pw")
+sql_user <- Sys.getenv("sql_user")
+sql_pw <- Sys.getenv("sql_pw")
 
 #log into database
 con <- dbConnect(RPostgres::Postgres(), dbname = 'Robinhood', host='localhost', port='5433', user= sql_user, password = sql_pw)
 
 #log into robinhood
 RH = RobinHood(username = rh_user, password = rh_pw) 
+
+
+
+#CREATE STOCK PRICE DATABASE
+
 #get list of tradeable tickers
 tickers = get_tickers(RH)
 tickers = filter(tickers, rhs_tradability == 'tradable' & state == 'active')
 
 tickers_good <- tibble(tickers$symbol)   #filter tickers
 
-num_groups = 25                 
+num_groups = 25      #create 25 groups for parallelization            
 
 #split the list of tickers into groups to avoid ram issues
 tickers_list <- tickers_good %>% 
@@ -73,3 +85,31 @@ for(i in 1:length(tickers_list)){
   tickers_tmp <- tibble(tickers_tmp)
   Get_Data(tickers_tmp)
 }
+
+
+#CALCULATE STOP LOSS VALUES
+
+#get positions
+positions <- get_positions(RH)
+
+#get list of symbols currently held
+symb <- positions$symbol
+names(symb) <- symb   #assign names to list
+
+#function to calculate stop losses using high - 3*ATR  (21 day)
+atr_trailing_stop <- function(symbol){
+  hlc <- dbGetQuery(con, paste0('SELECT high, low, close FROM "Stock_Data" WHERE symbol =', " '", symbol, "'", 'ORDER BY "date" ASC'))
+  hlc <- na.omit(hlc)
+  atr <-ATR(tail(hlc, 50), n = 21)
+  atr_tibble <- as_tibble(atr)
+  last(atr_tibble$trueHigh) - last(atr_tibble$atr)*3
+}
+
+stops <- lapply(symb, atr_trailing_stop)   #apply atr function to list
+stops_tbl <- as_tibble(stops)     #convert to table
+
+try(dbExecute(con, 'DROP TABLE "Stops"'), silent = TRUE) #delete old table query
+dbWriteTable(con, name = "Stops", value = stops_tbl, append = TRUE)  #write stops to sql table
+
+
+
