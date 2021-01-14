3b177aeeac076064174dc4b50a37bad70f877447
tylerlowe <tylerlowe1998@gmail.com>
2021-01-13 21:34:17 -0500

    Update Downloads_and_Computations.R

--- Downloads_and_Computations.R
+++ Downloads_and_Computations.R
@@ -44,8 +44,40 @@ tickers_list <- tickers_good %>%
   group_by((row_number()-1) %/% (n()/num_groups)) %>%
   nest %>% pull(data)
 
+#TTR functions with error handling
 #rsi function with error handling
 rsi_fun <- possibly(RSI, otherwise = NA)
+aroon_fun <- possibly(aroon, otherwise = NA)
+cci_fun <- possibly(CCI, otherwise = NA)
+obv_fun <- possibly(OBV, otherwise = NA)
+adx_fun <- possibly(ADX, otherwise = NA)
+atr_fun <- possibly(ATR, otherwise = NA)
+bbands_fun <- possibly(BBands, otherwise = NA)
+#come back for chaikin ad
+chaikinvol_fun <- possibly(chaikinVolatility, otherwise = NA)
+clv_fun <- possibly(CLV, otherwise = NA)
+#come back for cmf
+cmo_fun <- possibly(CMO, otherwise = NA)
+#come back for cti
+donachianchannel_fun <- possibly(DonchianChannel, otherwise = NA)
+dpo_fun <- possibly(DPO, otherwise = NA)
+#come back for dvi
+#come back for emv
+gmma_fun <- possibly(GMMA, otherwise = NA)
+kst_fun <- possibly(KST, otherwise = NA)
+macd_fun <- possibly(MACD, otherwise = NA)
+#come back for mfi
+pbands_fun <- possibly(PBands, otherwise = NA)
+sar_fun <- possibly(SAR, otherwise = NA)
+stoch_fun <- possibly(stoch, otherwise = NA)
+tdi_fun <- possibly(TDI, otherwise = NA)
+trix_fun <- possibly(TRIX, otherwise = NA)
+ultimateOscillator_fun <- possibly(ultimateOscillator, otherwise = NA)
+vhf_fun <- possibly(VHF, otherwise = NA)
+volatility_fun <- possibly(volatility, otherwise = NA)
+williamsAD_fun <- possibly(williamsAD, otherwise = NA)
+wpr_fun <- possibly(WPR, otherwise = NA)
+zigzag_fun <- possibly(ZigZag, otherwise = NA)
 
 #detect cores
 cl <- detectCores() - 1  
@@ -58,7 +90,7 @@ Get_Data <- function(tickers_good) {
   group <- rep(1:cl, length.out = nrow(tickers_good))
   tickers_good <- bind_cols(tibble(group), tickers_good)    
   #create clusters
-  cluster <- new_cluster(cl-1)     
+  cluster <- new_cluster(cl)     
   #partition by group
   by_group <- tickers_good %>% group_by(group) %>% partition(cluster)
   #setup clusers
@@ -83,9 +115,38 @@ Get_Data <- function(tickers_good) {
   #add rsi
   stockdata <- stockdata %>%
     group_by(symbol) %>%
-    group_map(~mutate(., rsi = rsi_fun(adjusted))) %>%
-    reduce(full_join)
-  
+    mutate(rsi_price = adjusted %>% na.approx(na.rm = FALSE) %>% rsi_fun(),
+           rsi_volume = volume %>% na.approx(na.rm = FALSE) %>% rsi_fun(),
+           cci = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% cci_fun(),
+           obv = obv_fun(adjusted, volume),
+           adx = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% adx_fun() %>% as.data.frame(),
+           aroon = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% aroon_fun() %>% as.data.frame(),
+           atr = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% atr_fun() %>% as.data.frame(),
+           bbands = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% bbands_fun() %>% as.data.frame(),
+           chaikinVol = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% chaikinvol_fun(),
+           clv = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% clv_fun(),
+           cmo_price = adjusted %>% na.approx(na.rm = FALSE) %>% cmo_fun(),
+           cmo_volume = volume %>% na.approx(na.rm = FALSE) %>% cmo_fun(),
+           donchannel = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% donachianchannel_fun() %>% as.data.frame(), 
+           dpo_price = adjusted %>% na.approx(na.rm = FALSE) %>% dpo_fun(),
+           dpo_volume = volume %>% na.approx(na.rm = FALSE) %>% dpo_fun(),
+           gmma_price = adjusted %>% na.approx(na.rm = FALSE) %>% gmma_fun(),
+           gmma_volume = volume %>% na.approx(na.rm = FALSE) %>% gmma_fun(),
+           kst = adjusted %>% na.approx(na.rm = FALSE) %>% kst_fun(),
+           macd_price = adjusted %>% na.approx(na.rm = FALSE) %>% macd_fun() %>% as.data.frame(),
+           macd_volume = volume %>% na.approx(na.rm = FALSE) %>% macd_fun() %>% as.data.frame(),
+           pbands = adjusted %>% na.approx(na.rm = FALSE) %>% pbands_fun() %>% as.data.frame(),
+           sar = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% sar_fun(),
+           stoch = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% stoch_fun() %>% as.data.frame(),
+           tdi = adjusted %>% na.approx(na.rm = FALSE) %>% tdi_fun() %>% as.data.frame(),
+           trix = adjusted %>% na.approx(na.rm = FALSE) %>% trix_fun(),
+           ultOscillator = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% ultimateOscillator_fun(), 
+           vhf = adjusted %>% na.approx(na.rm = FALSE) %>% vhf_fun(),
+           volatility = tibble(open, high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% volatility_fun(),
+           williamsAD = tibble(open, high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% williamsAD_fun(),
+           wpr = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% wpr_fun(),
+           zigzag = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% zigzag_fun()
+    )
   #Push to SQL
   dbWriteTable(con, name = "Stock_Data", value = stockdata, append = TRUE)
 }
