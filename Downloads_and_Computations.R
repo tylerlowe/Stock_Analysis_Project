6b721ee4167326ac8eb0a111113b3debdd52b877
tylerlowe <tylerlowe1998@gmail.com>
2021-01-23 14:45:33 -0500

    Updates to calculate indicators without memory issues

--- Downloads_and_Computations.R
+++ Downloads_and_Computations.R
@@ -16,35 +16,14 @@ library(tibble)
 library(TTR)
 library(furrr)
 
-rh_user <- Sys.getenv("rh_user")
-rh_pw <- Sys.getenv("rh_pw")
 sql_user <- Sys.getenv("sql_user")
 sql_pw <- Sys.getenv("sql_pw")
 
 #log into database
 con <- dbConnect(RPostgres::Postgres(), dbname = 'Robinhood', host='localhost', port='5433', user= sql_user, password = sql_pw)
 
-#log into robinhood
-RH = RobinHood(username = rh_user, password = rh_pw) 
 
 
-
-#CREATE STOCK PRICE DATABASE
-
-#get list of tradeable tickers
-tickers = get_tickers(RH)
-tickers = filter(tickers, rhs_tradability == 'tradable' & state == 'active')
-
-tickers_good <- tibble(tickers$symbol)   #filter tickers
-
-num_groups = 25      #create 25 groups for parallelization            
-
-#split the list of tickers into groups to avoid ram issues
-tickers_list <- tickers_good %>% 
-  group_by((row_number()-1) %/% (n()/num_groups)) %>%
-  nest %>% pull(data)
-
-#TTR functions with error handling
 #rsi function with error handling
 rsi_fun <- possibly(RSI, otherwise = NA)
 aroon_fun <- possibly(aroon, otherwise = NA)
@@ -79,14 +58,14 @@ williamsAD_fun <- possibly(williamsAD, otherwise = NA)
 wpr_fun <- possibly(WPR, otherwise = NA)
 zigzag_fun <- possibly(ZigZag, otherwise = NA)
 
-#detect cores
-cl <- detectCores() - 1  
-
 from <- "1900-01-01"
-to   <- today()
+to <- today()
+cl <- detectCores() - 1
 
+tic()
 
 Get_Data <- function(tickers_good) {
+  tickers_good <- data.frame(tickers_good)
   group <- rep(1:cl, length.out = nrow(tickers_good))
   tickers_good <- bind_cols(tibble(group), tickers_good)    
   #create clusters
@@ -96,111 +75,64 @@ Get_Data <- function(tickers_good) {
   #setup clusers
   by_group$cluster %>% cluster_library("purrr") %>% cluster_library("tidyquant") %>% cluster_assign("from" = from) %>%   cluster_assign("to" = to)
   #run parallelized code
-  stockdata <- by_group %>%
+  by_group %>%
     mutate(
       stock.prices = map(`tickers.symbol`,
-                                function(.x) tq_get(.x,
-                                                    get  = "stock.prices",
-                                                    from = from,
-                                                    to   = to,
-                                                    complete_cases = TRUE)
+                         function(.x) tq_get(.x,
+                                             get  = "stock.prices",
+                                             from = from,
+                                             to   = to,
+                                             complete_cases = TRUE)
       )
     ) %>%
     collect() %>% 
     unnest() %>%
     arrange(`tickers.symbol`) %>%
-    ungroup()
-  
-  stockdata$log_ret_adj <- calc.ret(stockdata$adjusted, tickers = stockdata$symbol, type.return = 'log') #calculate log returns
-  
-  #add rsi
-  stockdata <- stockdata %>%
+    ungroup() %>%
+    subset(select = c(group, symbol, date, open, high, low, close, volume, adjusted)) %>%
+    drop_na() %>%
     group_by(symbol) %>%
-    mutate(rsi_price = adjusted %>% na.approx(na.rm = FALSE) %>% rsi_fun(),
-           rsi_volume = volume %>% na.approx(na.rm = FALSE) %>% rsi_fun(),
-           cci = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% cci_fun(),
+    mutate(log_ret_adj = calc.ret(adjusted, tickers = symbol, type.return = 'log'),
+           rsi_price = adjusted  %>% rsi_fun(),
+           rsi_volume = volume  %>% rsi_fun(),
+           cci = tibble(high, low, adjusted)  %>% cci_fun(),
            obv = obv_fun(adjusted, volume),
-           adx = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% adx_fun() %>% as.data.frame(),
-           aroon = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% aroon_fun() %>% as.data.frame(),
-           atr = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% atr_fun() %>% as.data.frame(),
-           bbands = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% bbands_fun() %>% as.data.frame(),
-           chaikinVol = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% chaikinvol_fun(),
-           clv = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% clv_fun(),
-           cmo_price = adjusted %>% na.approx(na.rm = FALSE) %>% cmo_fun(),
-           cmo_volume = volume %>% na.approx(na.rm = FALSE) %>% cmo_fun(),
-           donchannel = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% donachianchannel_fun() %>% as.data.frame(), 
-           dpo_price = adjusted %>% na.approx(na.rm = FALSE) %>% dpo_fun(),
-           dpo_volume = volume %>% na.approx(na.rm = FALSE) %>% dpo_fun(),
-           gmma_price = adjusted %>% na.approx(na.rm = FALSE) %>% gmma_fun(),
-           gmma_volume = volume %>% na.approx(na.rm = FALSE) %>% gmma_fun(),
-           kst = adjusted %>% na.approx(na.rm = FALSE) %>% kst_fun(),
-           macd_price = adjusted %>% na.approx(na.rm = FALSE) %>% macd_fun() %>% as.data.frame(),
-           macd_volume = volume %>% na.approx(na.rm = FALSE) %>% macd_fun() %>% as.data.frame(),
-           pbands = adjusted %>% na.approx(na.rm = FALSE) %>% pbands_fun() %>% as.data.frame(),
-           sar = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% sar_fun(),
-           stoch = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% stoch_fun() %>% as.data.frame(),
-           tdi = adjusted %>% na.approx(na.rm = FALSE) %>% tdi_fun() %>% as.data.frame(),
-           trix = adjusted %>% na.approx(na.rm = FALSE) %>% trix_fun(),
-           ultOscillator = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% ultimateOscillator_fun(), 
-           vhf = adjusted %>% na.approx(na.rm = FALSE) %>% vhf_fun(),
-           volatility = tibble(open, high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% volatility_fun(),
-           williamsAD = tibble(open, high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% williamsAD_fun(),
-           wpr = tibble(high, low, adjusted) %>% na.approx(na.rm = FALSE) %>% wpr_fun(),
-           zigzag = tibble(high, low) %>% na.approx(na.rm = FALSE) %>% zigzag_fun()
-    ) %>% 
-    ungroup()
-  
-  stockdata <- do.call("data.frame", stockdata)   #unnest dataframe variables
-  
-  #Push to SQL
-  dbWriteTable(con, name = "Stock_Data", value = stockdata, append = TRUE)
+           adx = tibble(high, low, adjusted)  %>% adx_fun() %>% as.data.table(),
+           aroon = tibble(high, low)  %>% aroon_fun() %>% as.data.table(), 
+           atr = tibble(high, low, adjusted)  %>% atr_fun() %>% as.data.table(),
+           bbands = tibble(high, low, adjusted)  %>% bbands_fun() %>% as.data.table(),
+           chaikinVol = tibble(high, low)  %>% chaikinvol_fun() %>% as.data.table(),
+           clv = tibble(high, low, adjusted)  %>% clv_fun() %>% as.data.table(),
+           cmo_price = adjusted  %>% cmo_fun() %>% as.data.table(),
+           cmo_volume = volume  %>% cmo_fun() %>% as.data.table(),
+           donchannel = tibble(high, low)  %>% donachianchannel_fun() %>% as.data.table(), 
+           dpo_price = adjusted  %>% dpo_fun() %>% as.data.table(),
+           dpo_volume = volume  %>% dpo_fun() %>% as.data.table(),
+           gmma_price = adjusted  %>% gmma_fun() %>% as.data.table(),
+           gmma_volume = volume  %>% gmma_fun() %>% as.data.table(),
+           kst = adjusted %>% kst_fun() %>% as.data.table(),
+           macd_price = adjusted  %>% macd_fun() %>% as.data.table(),
+           macd_volume = volume %>% macd_fun() %>% as.data.table(),
+           pbands = adjusted %>% pbands_fun() %>% as.data.table(),
+           sar = tibble(high, low) %>% sar_fun() %>% as.data.table(),
+           stoch = tibble(high, low, adjusted) %>% stoch_fun() %>% as.data.table(),
+           tdi = adjusted %>% tdi_fun() %>% as.data.table(),
+           trix = adjusted %>% trix_fun() %>% as.data.table(),
+           ultOscillator = tibble(high, low, adjusted) %>% ultimateOscillator_fun() %>% as.data.table(), 
+           vhf = adjusted %>% vhf_fun() %>% as.data.table(),
+           volatility = tibble(open, high, low, adjusted) %>% volatility_fun() %>% as.data.table(),
+           williamsAD = tibble(open, high, low, adjusted) %>% williamsAD_fun() %>% as.data.table(),
+           wpr = tibble(high, low, adjusted) %>% wpr_fun() %>% as.data.table(),
+           zigzag = tibble(high, low) %>% zigzag_fun() %>% as.data.table()
+    ) %>%
+    ungroup() %>%
+    do.call(what = data.table, .) %>%  #unnest dataframe variables
+    Filter(f = function(x)!all(is.na(x))) %>%
+    dbWriteTable(conn = con, name = "Stock_Data", value = ., append = TRUE)  #Push to SQL
 }
 
-tic()
+i <- read.table("iteration.txt")
 
-
-
-try(dbExecute(con, 'DROP TABLE "Stock_Data"'), silent = TRUE) #delete old table query
-
-
-#map functions to list of tickers
-
-
-fix_ticker_list<- function(tickers){
-  tickers_tmp <- data.frame(tickers)   
-  tickers_tmp <- tibble(tickers_tmp)
-  return(tickers_tmp)
-}
-
-tickers_list_fixed <- map(tickers_list, fix_ticker_list)
-
-map(tickers_list_fixed, Get_Data)
+Get_Data(i)
 
 toc()
-
-#CALCULATE STOP LOSS VALUES
-tic()
-#get positions
-positions <- get_positions(RH)
-
-#get list of symbols currently held
-symb <- positions$symbol
-names(symb) <- symb   #assign names to list
-
-#function to calculate stop losses using high - 3*ATR  (21 day)
-atr_trailing_stop <- function(symbol){
-  hlc <- dbGetQuery(con, paste0('SELECT high, low, close FROM "Stock_Data" WHERE "tickers.symbol" =', " '", symbol, "'", 'ORDER BY "date" ASC'))
-  hlc <- na.omit(hlc)
-  atr <-ATR(tail(hlc, 50), n = 21)
-  atr_tibble <- as_tibble(atr)
-  last(atr_tibble$trueHigh) - last(atr_tibble$atr)*3
-}
-
-stops <- lapply(symb, atr_trailing_stop)   #apply atr function to list
-stops_tbl <- as_tibble(stops)     #convert to table
-
-try(dbExecute(con, 'DROP TABLE "Stops"'), silent = TRUE) #delete old table query
-dbWriteTable(con, name = "Stops", value = stops_tbl, append = TRUE)  #write stops to sql table
-
-toc()
-
---
+++ Execute_Downloads.R
@@ -0,0 +1,42 @@
+library(dplyr)
+library(RobinHood)
+library(RPostgres)
+library(tidyr)
+library(tictoc)
+
+
+rh_user <- Sys.getenv('rh_user')
+rh_pw <- Sys.getenv('rh_pw')
+sql_user <- Sys.getenv('sql_user')
+sql_pw <- Sys.getenv('sql_pw')
+
+#log into database
+con <- dbConnect(RPostgres::Postgres(), dbname = 'Robinhood', host='localhost', port='5433', user= sql_user, password = sql_pw)
+
+#log into robinhood
+RH = RobinHood(username = rh_user, password = rh_pw)
+
+tickers = get_tickers(RH)
+
+
+tickers = filter(tickers, rhs_tradability == 'tradable' & state == 'active')
+
+tickers_good <- tibble(tickers$symbol)  #filter tickers
+
+num_groups = 35     #create 35 groups for parallelization            
+
+#split the list of tickers into groups to avoid ram issues
+tickers_list <- tickers_good %>% 
+  group_by((row_number()-1) %/% (n()/num_groups)) %>%
+  nest %>% pull(data)
+
+
+try(dbExecute(con, 'DROP TABLE "Stock_Data"'), silent = TRUE) #delete old table query
+
+for (i in 1:length(tickers_list)) {
+  tic()
+  write.table(data.frame(tickers_list[i]), "iteration.txt")
+  system("/usr/lib/R/bin/Rscript '/home/tyler/Documents/GitHub/Stock_Analysis_Project/Downloads_and_Computations.R'")
+  print(paste0("Iteration ", i))
+  toc()
+}
